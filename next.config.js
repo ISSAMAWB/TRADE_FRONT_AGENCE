0a0bea8584b2344c5315e43d3833db6623b9bf04
MOHSSINEAWB <m.gharmoud@attijariwafa.com>
2026-07-07 17:43:09 +0100

    deploy-github-pages-statique

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true };
+const nextConfig = {
+  reactStrictMode: true,
+  output: "export",
+  distDir: "dist",
+  images: {
+    unoptimized: true,
+  },
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    const onlyStatic = Object.fromEntries(
+      Object.entries(defaultPathMap).filter(([path]) => !path.includes("["))
+    );
+    return onlyStatic;
+  },
+};
+
 module.exports = nextConfig;
